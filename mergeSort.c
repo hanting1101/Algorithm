f7b819cc95a699f0f336dc80bb6dda27883e2456
hanting1101 <100844680+hanting1101@users.noreply.github.com>
2024-03-13 21:38:46 +0800

    Add files via upload

---
+++ mergeSort.c
@@ -0,0 +1,105 @@
+// C program for Merge Sort
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r){
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	// Create temp arrays
+	int *L = (int*)malloc((n1 + 1) * sizeof(int));
+    int *R = (int*)malloc((n2 + 1) * sizeof(int));
+
+	// Copy data to temp arrays L[] and R[]
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+    L[n1] = INT_MAX;R[n2] = INT_MAX;
+
+	// Merge the temp arrays back into arr[l..r
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of L[],
+	// if there are any
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of R[],
+	// if there are any
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+    free(L);
+    free(R);
+}
+
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+void mergeSort(int arr[], int l, int r){
+	if (l < r) {
+		int m = l + (r - l) / 2;
+
+		// Sort first and second halves
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
+}
+
+// Function to print an array
+void printArray(int A[], int size){
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+}
+
+int main(){
+    int arr_size;
+    int i = 0;
+    printf("Number of array: ");
+    scanf("%d", &arr_size);
+    int *arr = (int*)malloc(arr_size * sizeof(int));
+    while(i < arr_size){
+        scanf("%d", &arr[i]);
+        i++;
+    }
+
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size);
+	return 0;
+
+    free(arr);
+}
